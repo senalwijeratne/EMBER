ed55d0b531d3053179d52dd89daafd945bc1b339
Mohamed Ikhwan <1khwan.m0@gmail.com>
2017-04-26 17:08:09 +0530

    Added back Password to user file

--- EmberLara/database/migrations/2014_10_12_000000_create_users_table.php
+++ EmberLara/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('password');
+            $table->string('passwordConfirmation');
             $table->string('firstName');
             $table->string('middleName');
             $table->string('lastName');

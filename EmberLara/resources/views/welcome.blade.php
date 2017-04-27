0ff155e99ffec87d99fe1b48b931b1913aed62ed
Senal Wijeratne <senal.ra128@gmail.com>
2017-04-27 11:39:34 +0530

    title and icon

--- EmberLara/resources/views/welcome.blade.php
+++ EmberLara/resources/views/welcome.blade.php
@@ -154,6 +154,8 @@
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-light_blue.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+    <link rel="shortcut icon" href="{{asset('img/ember.png')}}">
+    <title>EMBER</title>
 
 </head>
 
